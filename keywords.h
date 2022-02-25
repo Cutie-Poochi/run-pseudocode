e18eac6c5e5daefaacfe7f470c7c0301848f4910
Cutie-Poochi <cutielanders@gmail.com>
2022-02-25 20:08:17 +0600

    First version

---
+++ keywords.h
@@ -0,0 +1,137 @@
+#include <string>
+#include <unordered_set>
+
+namespace Keyword {
+	const std::string IF = "IF";
+	const std::string REPEAT = "REPEAT";
+	const std::string WHILE = "WHILE";
+	const std::string FOR = "FOR";
+	const std::string DO = "DO";
+	const std::string THEN = "THEN";
+	const std::string ELSE = "ELSE";
+	const std::string UNTIL = "UNTIL";
+	const std::string TO = "TO";
+	const std::string STEP = "STEP";
+	const std::string NEXT = "NEXT";
+	const std::string INPUT = "INPUT";
+	const std::string OUTPUT = "OUTPUT";
+	const std::string END = "END";
+	const std::string ASSIGN = "<-";
+	const std::string S_ADD = "+";
+	const std::string S_SUB = "-";
+	const std::string S_MULT = "*";
+	const std::string S_DIV = "/";
+	const std::string S_POW = "^";
+	const std::string PROCEDURE = "PROCEDURE";
+	const std::string CALL = "CALL";
+	const std::string COLON = ":";
+	const std::string FUNCTION = "FUNCTION";
+	const std::string RETURN = "RETURN";
+	const std::string RETURNS = "RETURNS";
+	const std::string INTEGRE = "INTEGRE";
+	const std::string REAL = "REAL";
+	const std::string CHAR = "CHAR";
+	const std::string STRING = "STRING";
+	const std::string BOOLEAN = "BOOLEAN";
+	const std::string ARRAY = "ARRAY";
+	const std::string OF = "OF";
+	const std::string DECLARE = "DECLARE";
+	const std::string CONSTANT = "CONSTANT";
+	const std::string DIV = "DIV";
+	const std::string MOD = "MOD";
+	const std::string C_EQ = "==";
+	const std::string C_LT = "<";
+	const std::string C_LE = "<=";
+	const std::string C_GT = ">";
+	const std::string C_GE = ">=";
+	const std::string C_NE = "<>";
+	const std::string O_PS = "(";
+	const std::string O_PE = ")";
+	const std::string O_BS = "[";
+	const std::string O_BE = "]";
+	const std::string SINGQUOTES = "'";
+	const std::string DOUBQUOTES = "\"";
+	const std::string LENGTH = "LENGTH";
+	const std::string UCASE = "UCASE";
+	const std::string LCASE = "LCASE";
+	const std::string SUBSTRING = "SUBSTRING";
+	const std::string ROUND = "ROUND";
+	const std::string RANDOM = "RANDOM";
+	const std::string CASE = "CASE";
+	const std::string OTHERWISE = "OTHERWISE";
+	const std::string OPENFILE = "OPENFILE";
+	const std::string READ = "READ";
+	const std::string WRITE = "WRITE";
+	const std::string READFILE = "READFILE";
+	const std::string WRITEFILE = "WRITEFILE";
+	const std::string CLOSEFILE  = "CLOSEFILE ";
+
+	// not to be parsed, just identifiers ig
+	const std::string COMMENT = "//";
+	const std::string UNKNOWN = "?";
+	const std::string NEWLINE = "\n";
+	const std::string VARIABLE = "var";
+}
+std::unordered_set<std::string> keywords = {
+	Keyword::IF,
+	Keyword::REPEAT,
+	Keyword::WHILE,
+	Keyword::FOR,
+	Keyword::DO,
+	Keyword::THEN,
+	Keyword::ELSE,
+	Keyword::UNTIL,
+	Keyword::TO,
+	Keyword::STEP,
+	Keyword::NEXT,
+	Keyword::INPUT,
+	Keyword::OUTPUT,
+	Keyword::END,
+	Keyword::ASSIGN,
+	Keyword::S_ADD,
+	Keyword::S_SUB,
+	Keyword::S_MULT,
+	Keyword::S_DIV,
+	Keyword::S_POW,
+	Keyword::PROCEDURE,
+	Keyword::CALL,
+	Keyword::COLON,
+	Keyword::FUNCTION,
+	Keyword::RETURN,
+	Keyword::RETURNS,
+	Keyword::INTEGRE,
+	Keyword::REAL,
+	Keyword::CHAR,
+	Keyword::STRING,
+	Keyword::BOOLEAN,
+	Keyword::ARRAY,
+	Keyword::OF,
+	Keyword::DECLARE,
+	Keyword::CONSTANT,
+	Keyword::DIV,
+	Keyword::MOD,
+	Keyword::C_EQ,
+	Keyword::C_LT,
+	Keyword::C_LE,
+	Keyword::C_GT,
+	Keyword::C_GE,
+	Keyword::C_NE,
+	Keyword::O_PS,
+	Keyword::O_PE,
+	Keyword::O_BS,
+	Keyword::O_BE,
+	Keyword::LENGTH,
+	Keyword::UCASE,
+	Keyword::LCASE,
+	Keyword::SUBSTRING,
+	Keyword::ROUND,
+	Keyword::RANDOM,
+	Keyword::CASE,
+	Keyword::OTHERWISE,
+	Keyword::OPENFILE,
+	Keyword::READ,
+	Keyword::WRITE,
+	Keyword::READFILE,
+	Keyword::WRITEFILE,
+	Keyword::CLOSEFILE
+};
---
+++ pseudocode.cpp
@@ -0,0 +1,129 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include "keywords.h"
+
+class Token
+{
+public:
+    std::string type;
+    std::string value;
+
+    Token(const std::string& s)
+    {
+		if (keywords.find(s) != keywords.end()) {
+			type = s;
+			return;
+		}
+		value = s;
+		char* fail;
+		strtol(s.c_str(), &fail, 10);
+		if (!*fail) {
+			type = Keyword::INTEGRE;
+			return;
+		}
+		strtod(s.c_str(), &fail);
+		if (!*fail) {
+			type = Keyword::REAL;
+			return;
+		}
+		type = Keyword::UNKNOWN;
+    }
+
+    Token(const std::string& type, const std::string& value)
+        : type(type), value(value)
+    {
+    }
+};
+
+std::ostream &operator<<(std::ostream& os, const Token& token) {
+    if (token.value.empty())
+        return os << '(' << token.type << ')';
+    return os << '(' << token.type << ": " << token.value << ')';
+}
+
+std::vector<std::vector<Token>> parse(std::ifstream& sourceFile)
+{
+	std::vector<std::vector<Token>> allTokens;
+    std::string line;
+    while (std::getline(sourceFile, line))
+	{
+        size_t comment_index = line.find(Keyword::COMMENT);
+        if (comment_index != std::string::npos)
+            line = line.substr(0, comment_index);
+
+        std::vector<Token> lineTokens;
+        std::string currentTokenString;
+        for (char letter : line)
+        {
+            switch(letter) {
+                case ' ':
+                    if (!currentTokenString.empty()) {
+                        lineTokens.push_back(currentTokenString);
+                        currentTokenString.clear();
+                    }
+                    break;
+                default:
+                    currentTokenString.push_back(letter);
+            }
+        }
+        if (!currentTokenString.empty())
+            lineTokens.push_back(currentTokenString);
+		
+		if (lineTokens.size() > 0)
+			allTokens.push_back(lineTokens);
+    }
+
+    return allTokens;
+}
+
+void evaluate(std::vector<std::vector<Token>>& tokens) {
+	std::map<std::string, Token> variables;
+	for (auto lineTokens : tokens)
+	{
+		// variable <- value
+		if (lineTokens[1].type == Keyword::ASSIGN)
+		{
+			if (lineTokens[0].type != Keyword::UNKNOWN || (lineTokens[2].type != Keyword::STRING && lineTokens[2].type != Keyword::INTEGRE &&lineTokens[2].type != Keyword::BOOLEAN))
+			{
+			}
+			variables.insert(std::pair<std::string, Token>(lineTokens[0].value, lineTokens[2]));
+		}
+		// OUTPUT printable
+		if (lineTokens[0].type == Keyword::OUTPUT)
+		{
+			// OUTPUT variable
+			if (lineTokens[1].type == Keyword::UNKNOWN) {
+				std::map<std::string, Token>::const_iterator pos = variables.find(lineTokens[1].value);
+				if (pos != variables.end())
+					std::cout << pos->second.value << '\n';
+			}
+		}
+	}
+	return;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "Give a file\n";
+        return 0;
+    }
+    std::ifstream sourceFile;
+    sourceFile.open(argv[1]);
+    if (!sourceFile.is_open()) {
+        std::cout << "File doesn't exist\n";
+        return 0;
+    }
+
+    auto tokens = parse(sourceFile);
+	// for (auto line : tokens) {
+		// for (auto token : line)
+			// std::cout << token << ' ';
+		// std::cout << '\n';
+	// }
+	// std::cout << "\n\n";
+	evaluate(tokens);
+    sourceFile.close();
+}
